ff36f2bdcba3c10f75c073cda980966bbddea6af
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2020-11-09 10:48:19 -0800

    Add search

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,36 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <header>
+    <span id="site-title">Watch Tracker</span>
+
+    <Search></Search>
+  </header>
+  <router-view :key="$route.fullPath"/>
 </template>
+
+<script>
+import Search from '@/components/Search.vue';
+
+export default {
+  components: {
+    Search,
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
-#nav {
+#site-title {
+  font-size: 14px;
+  font-weight: bold;
+}
+
+#main-nav {
   padding: 30px;
 
   a {
---
+++ src/components/Search.vue
@@ -0,0 +1,130 @@
+<template>
+  <form class="search">
+    <input type="text" v-model="query" v-on:keyup="search">
+    <ul class="results" v-if="query">
+      <li><span class="type">TV Shows</span>
+        <ul>
+          <li v-for="result in results.shows" v-bind:key="result.id"><router-link v-bind:to="'/shows/' + result.id">{{ result.name }}</router-link></li>
+        </ul>
+      </li>
+      <li><span class="type">Movies</span>
+        <ul>
+          <li v-for="result in results.movies" v-bind:key="result.id"><router-link v-bind:to="'/movies/' + result.id">{{ result.title }}</router-link></li>
+        </ul>
+      </li>
+      <li><span class="type">People</span>
+        <ul>
+          <li v-for="result in results.people" v-bind:key="result.id"><router-link v-bind:to="'/people/' + result.id">{{ result.name }}</router-link></li>
+        </ul>
+      </li>
+    </ul>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+const apiKey = process.env.VUE_APP_API_KEY;
+
+export default {
+  name: 'Search',
+  data() {
+    return {
+      query: '',
+      results: {},
+    };
+  },
+  methods: {
+    search() {
+      this.results = {
+        movies: [],
+        people: [],
+        shows: [],
+      };
+
+      if (this.query) {
+        const config = {
+          params: {
+            api_key: apiKey,
+            query: this.query,
+          },
+        };
+
+        axios
+          .get('https://api.themoviedb.org/3/search/multi', config)
+          .then((response) => {
+            response.data.results.forEach((result) => {
+              if (result.media_type === 'tv') {
+                this.results.shows.push(result);
+              } else if (result.media_type === 'movie') {
+                this.results.movies.push(result);
+              } else if (result.media_type === 'person') {
+                this.results.people.push(result);
+              }
+            });
+          });
+      } else {
+        this.results = [];
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.search {
+  display: inline-block;
+  position: relative;
+}
+
+.results {
+  background-color: #fff;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.2);
+  display: none;
+  min-width: 250px;
+  padding: 10px 0;
+  position: absolute;
+  top: 30px;
+
+  li {
+    list-style-type: none;
+    margin: 0;
+  }
+
+  ul {
+    padding: 0;
+  }
+
+  li li a {
+    color: #000;
+    display: block;
+    padding: 5px 40px;
+    text-decoration: none;
+
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.1);
+      cursor: pointer;
+    }
+  }
+}
+
+.search input:focus + .results,
+.results:active {
+  display: block;
+}
+
+.type {
+  display: block;
+  font-weight: bold;
+  padding: 5px 20px;
+}
+
+input {
+  border: 1px solid #ddd;
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1rem;
+  margin: 0 10px;
+  padding: 8px 10px;
+}
+</style>
